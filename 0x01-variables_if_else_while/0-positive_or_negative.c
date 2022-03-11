bbf96fd1b531919e9b0ebe2643c1509e19768bea
jositedy <josit476@gmail.com>
2022-03-11 12:04:00 -0800

    less than

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include<stdio.h>
+
+void main()
+{
+	int n,0;
+
+	if (n>0)
+	{
+		printf("\n n is greater than 0");
+	}
+	else if (n<0)
+	{
+		printf("\n 0 is greater than n");
+	}
+	else
+	}
