e8dc8c0ab466500f864de799478747993c5253ef
jositedy <josit476@gmail.com>
2022-03-24 21:33:02 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,23 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
-char *_strncat(char *dest, char *src, int n)
+
+int _strcmp(char *s1, char *s2)
 {
-	int count = 0, count2 = 0;
+	int i = 0, op = 0;
 
-	while (*(dest + count) != '\0')
+	while (op == 0)
 	{
-		count++;
-	}
-
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		count++;
-		count2++;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	return (dest);
+
+	return (op);
 }
